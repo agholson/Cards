752e05424888fa812c4b55d1d44b4860cf56b77d
agholson <87455899+agholson@users.noreply.github.com>
2022-07-12 20:32:33 -0400

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Leone on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
