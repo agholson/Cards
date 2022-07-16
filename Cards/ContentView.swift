41eaea67be339d2afe3fcd01e7751ac9f56005ed
agholson <87455899+agholson@users.noreply.github.com>
2022-07-16 14:09:52 -0400

    Finished the card lesson :)

--- Cards/ContentView.swift
+++ Cards/ContentView.swift
@@ -8,9 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // transparent color used in the corner
+    let transparentColor = Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        GeometryReader { geo in
+            ZStack {
+                
+                Rectangle()
+                    .foregroundColor(.orange)
+                
+                Image("neroInTheDesert")
+                    .resizable()
+                    .scaledToFit()
+                
+                Circle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [.orange, transparentColor]), startPoint: .top, endPoint: .bottom))
+                    // Make smaller
+                    .frame(width: 200)
+                    .offset(x: (geo.size.width - 90)/2, y: (-geo.size.height)/2 - 40)
+                
+                // MARK: Content
+                VStack {
+                    
+                    HStack {
+                        Spacer()
+                        Text("In theaters soon...")
+                            .font(.caption)
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                    }
+                    .padding([.top, .leading])
+                    
+                    Spacer()
+                    // Use ZStack, so we can place labels on top
+                    ZStack (alignment: .leading) {
+                        // MARK: White rectangle
+                        Rectangle()
+                            .foregroundColor(.white)
+                            .frame(height: 100) // Makes 80 height at bottom
+                        
+                        VStack(alignment: .leading) {
+                            Text("The Brave cat")
+                                .font(.headline)
+                            Text("Who would have known man's only hope in the desert would have been a cat with sand-colored eyes?")
+                                .font(.caption)
+                        }
+                        .padding(.horizontal)
+                    }
+                }
+                
+            }
+        }
+        // Use a frame/ height to make the cards the same size
+        .frame(height: 550)
+        // Place the cornerRadius on here versus a rounded rectangle, because the clipped images in the roundedRectangle would not have that
+        .cornerRadius(15)
+        .shadow(radius: 10)
+        .padding()
     }
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+#  Cards in SwiftUI
+Project shows how to create a card, one of the most common design elements in iOS apps. From 
+the amazing tutorials at CodeWithChris.com: https://learn.codewithchris.com/courses/take/design/lessons/26070124-lesson-3-cards.
+
+# Design
+![Finished card design](design.jpeg)
+
